81ab335045dcbcde7934d7dc6a873ad361505f72
Michael Aufreiter <michael@substance.io>
2017-08-03 18:08:04 +0200

    Update fixture.js

--- app/fixture.js
+++ app/fixture.js
@@ -1,5 +1,5 @@
 export default `<?xml version="1.0"?>
-<!DOCTYPE article PUBLIC "SimpleArticle 1.1" "http://substance.io/SimpleArticle-1.0.dtd">
+<!DOCTYPE article PUBLIC "ExampleArticle 1.0" "http://substance.io/ExampleArticle-1.0.dtd">
 <example-article>
   <title></title>
   <body>
